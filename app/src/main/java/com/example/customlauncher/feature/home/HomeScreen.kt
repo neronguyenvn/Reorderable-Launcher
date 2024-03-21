87e5100287f13bbfd30240c908b2260d92debb08
Peter <hoangnm9a2@gmail.com>
2024-03-21 10:15:21 +0700

    scale gif

--- app/src/main/java/com/example/customlauncher/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/customlauncher/feature/home/HomeScreen.kt
@@ -1,7 +1,8 @@
 package com.example.customlauncher.feature.home
 
+import android.graphics.Bitmap
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,21 +13,20 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.AsyncImage
 import com.example.customlauncher.R
 import com.example.customlauncher.core.designsystem.component.reorderablelazygrid.ItemPosition
 import com.example.customlauncher.core.designsystem.component.reorderablelazygrid.ReorderableItem
@@ -44,6 +44,7 @@ import com.example.customlauncher.feature.home.HomeScreenEvent.StartDrag
 import com.example.customlauncher.feature.home.HomeScreenEvent.StopDrag
 import pl.droidsonroids.gif.GifDrawable
 import pl.droidsonroids.gif.GifImageView
+
 
 sealed interface HomeScreenEvent {
     data class LongClickOnApp(val userApp: UserApp?) : HomeScreenEvent
@@ -72,11 +73,10 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
             // Tạo GifDrawable từ tệp tin GIF
             val gifInputStream = LocalContext.current.resources.openRawResource(R.raw.loading)
             val gifDrawable = GifDrawable(gifInputStream)
-
             // Sử dụng GifImageViewWrapper để hiển thị GifImageView
             GifImageViewWrapper(
                 gifDrawable = gifDrawable,
-                modifier = Modifier.size(50.dp)
+                Modifier.scale(2f,2f)
             )
 
         }
